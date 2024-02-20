22db516a05552505bdbc028844806a0013527683
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2024-02-20 20:34:57 -0300

    Add files via upload

---
+++ DS/Revisão/ex006.java
@@ -0,0 +1,12 @@
+
+public class ex006 {
+	public static void main(String[] args) {
+		
+		int vetor[] = {3,2,8,7,5,4};
+		
+		for(int i=0; i<vetor.length; i++) {
+			System.out.println("Na posição "+i+" temos o valor "+vetor[i]);
+		}
+		
+	}
+}
