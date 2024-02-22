13fc6f9bccef267e1458a110600f07fe547781dc
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2024-02-22 17:57:20 -0300

    Update ex006.java

--- DS/Revisão/ex006.java
+++ DS/Revisão/ex006.java
@@ -1,8 +1,14 @@
-
+import java.util.Scanner;
 public class ex006 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		int vetor[] = {3,2,8,7,5,4};
+		int vetor[] = new int [6];
+		
+		for(int i=0; i<vetor.length; i++) {
+			System.out.println("Entre com o "+(i+1)+"º valor:");
+			vetor[i] = sc.nextInt();
+		}
 		
 		for(int i=0; i<vetor.length; i++) {
 			System.out.println("Na posição "+i+" temos o valor "+vetor[i]);
