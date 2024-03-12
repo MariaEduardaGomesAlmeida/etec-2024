d0dd667aa3d4997c9a4db838179e836c484c903e
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2024-03-12 16:18:15 -0300

    Add files via upload

---
+++ PW/IntroducaoNodejs/Modularizando/index.js
@@ -0,0 +1,10 @@
+import areaCircuferencia from "./area.js";
+import celsiusParaFahrenheit from "./graus.js";
+import verificarAnoBissexto from "./bissexto.js";
+import calcularQuadrado from "./quadrado.js";
+
+
+console.log("A área é de: "+areaCircuferencia(6).toFixed(2));
+console.log("A conversão de celsius para fahrenheit é: "+celsiusParaFahrenheit(30));
+console.log(verificarAnoBissexto(2024));
+console.log("O quadrado é: "+calcularQuadrado(5));
