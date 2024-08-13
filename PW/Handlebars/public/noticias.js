6ea426cbefcd6441725572d6326f1e2b65655169
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2024-08-12 22:58:05 -0300

    Add files via upload

---
+++ PW/Handlebars/public/noticias.js
@@ -0,0 +1,54 @@
+[
+    {
+        id: 1,
+        imagem: "/public/img/ceupagina1.jfif",
+        segundaImagem: "/public/img/foto1.jfif",
+        titulo: "Nova extensão",
+        conteudo: "Com o objetivo de fortalecer a educação profissionalizante e atender à crescente demanda por habilidades técnicas especializadas, está em andamento o planejamento para a inauguração de uma nova unidade de escola técnica na região. Este empreendimento educacional visa oferecer uma ampla gama de cursos práticos e teóricos, capacitando os alunos para ingressarem no mercado de trabalho com competências sólidas e atualizadas. A nova escola técnica será um centro de excelência em educação, equipado com laboratórios modernos, salas de aula tecnologicamente avançadas e uma equipe docente altamente qualificada. Os cursos oferecidos abrangerão diversas áreas, como tecnologia da informação, engenharia, saúde, administração, entre outras, proporcionando aos estudantes a oportunidade de escolherem o caminho que melhor se adequa aos seus interesses e aptidões.",
+        segundoParagrafo: "Além disso, a nova unidade de escola técnica buscará estreitar parcerias com empresas locais e instituições do setor, proporcionando aos alunos estágios, programas de aprendizagem prática e oportunidades de emprego após a conclusão dos cursos. Dessa forma, os estudantes poderão adquirir experiência real no ambiente de trabalho e desenvolver habilidades profissionais essenciais para o sucesso em suas carreiras. A inauguração desta nova unidade de escola técnica representa um marco significativo para a educação na região, oferecendo uma alternativa de qualidade e relevância para os jovens que buscam uma formação profissional sólida e perspectivas de emprego promissoras. Espera-se que essa iniciativa contribua não apenas para o desenvolvimento pessoal e profissional dos estudantes, mas também para o crescimento econômico e social da comunidade como um todo.",
+        autor: "Paula Silva",
+        data: "Data de Publicação: 3 de junho de 2024"
+    },
+    {
+        id: 2,
+        imagem: "/public/img/tecsesppagina2.jfif",
+        segundaImagem: "/public/img/foto2.jfif",
+        titulo: "TECSESP cancelada",
+        conteudo: " Com pesar, a comunidade escolar da Escola Técnica Estadual (TECSESP) foi informada sobre o cancelamento do evento esportivo anual, um dos momentos mais esperados do calendário escolar. Esta decisão difícil foi tomada devido a uma série de circunstâncias infelizes que culminaram na necessidade de uma nova direção. Após uma investigação minuciosa, veio à tona que a direção anterior da escola estava envolvida em atividades criminosas, comprometendo não apenas a integridade da instituição, mas também colocando em risco a segurança e o bem-estar dos alunos e funcionários. Diante desse cenário alarmante, medidas urgentes foram tomadas para restaurar a ordem e a confiança na gestão da escola.",
+        segundoParagrafo: " A transição para uma nova direção é um processo complexo que demanda tempo e cuidado. Enquanto isso, a prioridade máxima é garantir a segurança e o funcionamento adequado da escola. Nesse contexto, a realização do evento esportivo foi suspensa temporariamente até que a nova direção possa revisar e reestruturar o planejamento do evento, assegurando que todas as medidas de segurança e organização estejam em conformidade com os mais altos padrões. Embora compreendamos a decepção e o inconveniente que essa decisão possa causar, acreditamos firmemente que é crucial priorizar a integridade e a segurança de nossa comunidade escolar. Estamos comprometidos em trabalhar incansavelmente para restaurar a confiança e oferecer um ambiente educacional seguro, acolhedor e propício ao desenvolvimento integral de nossos alunos. Enquanto aguardamos ansiosamente pelo retorno do evento esportivo e por dias melhores para nossa escola, contamos com a compreensão e o apoio de todos os envolvidos. Juntos, superaremos esse desafio e construiremos um futuro promissor para a TECSESP.",
+        autor: "Roberto Gomes",
+        data: "Data de Publicação: 1 de janeiro de 2021"
+    },
+    {
+        id: 3,
+        imagem: "/public/img/eventospagina3.jfif",
+        segundaImagem: "/public/img/foto3.jfif",
+        titulo: "Eventos de junho",
+        conteudo: "Com a chegada do mês de junho, a Escola Técnica Estadual (ETEC) CT está repleta de expectativas e atividades emocionantes para os alunos, professores e toda a comunidade escolar. Este mês, além das aulas regulares e atividades curriculares, a ETEC CT está preparando uma série de eventos especiais para enriquecer a experiência educacional e promover o engajamento dos estudantes. Uma das principais atividades planejadas para este mês é a Feira de Ciências, um evento anual que destaca o talento e a criatividade dos alunos em projetos científicos inovadores. Neste ano, os estudantes da ETEC CT apresentarão uma variedade de projetos nas áreas de biologia, física, química, tecnologia e muito mais. A feira proporciona uma oportunidade única para os alunos demonstrarem suas habilidades de pesquisa, experimentação e comunicação, além de estimular o interesse pela ciência e pela tecnologia.",
+        segundoParagrafo: "Além da Feira de Ciências, a ETEC CT também está organizando uma série de palestras e workshops com profissionais renomados em diversas áreas do conhecimento. Essas atividades têm como objetivo enriquecer o aprendizado dos alunos, fornecendo insights valiosos sobre as tendências e oportunidades no mercado de trabalho, além de inspirar futuras carreiras e projetos acadêmicos.Outro destaque do mês de junho na ETEC CT é o torneio esportivo interclasses, uma competição saudável que promove o espírito de equipe, a disciplina e a atividade física entre os estudantes. Os alunos terão a oportunidade de participar de uma variedade de modalidades esportivas, incluindo futebol, basquete, vôlei e muito mais, demonstrando suas habilidades atléticas e competindo pelo orgulho de representar suas turmas. Além desses eventos, a ETEC CT continuará oferecendo suporte acadêmico e orientação profissional aos alunos, preparando-os para os desafios futuros e capacitando-os a alcançar seus objetivos pessoais e profissionais. Com uma programação diversificada e envolvente, o mês de junho promete ser repleto de aprendizado, diversão e realização na ETEC CT.",
+        autor: "Alice Maia",
+        data: "Data de Publicação: 10 de dezembro de 2022"
+    },
+    {
+        id: 4,
+        imagem: "/public/img/quadra.jfif",
+        segundaImagem: "/public/img/foto4.jfif",
+        titulo: "Jogos amistosos",
+        conteudo: "Os jogos amistosos de futebol entre os cursos de Data Science (DS), Administração (ADM), Química, Recursos Humanos (RH) e Informática na nossa escola foram emocionantes e demonstraram o espírito competitivo e a camaradagem entre os estudantes de diferentes áreas.",
+        segundoParagrafo: "No confronto entre os times de Recursos Humanos e Informática, testemunhamos um jogo emocionante, com muitos momentos de suspense e reviravoltas. Ambas as equipes demonstraram grande habilidade técnica e determinação, mas no final, o time de Informática se destacou, garantindo a vitória com uma atuação impressionante. Por fim, o confronto entre os times de Administração e Química foi um verdadeiro duelo de titãs. Ambas as equipes lutaram arduamente, mostrando um grande domínio do jogo e uma forte determinação em campo. No entanto, o time de Administração conseguiu impor sua estratégia e saiu vitorioso. Em suma, os jogos amistosos de futebol entre os cursos da nossa escola foram uma oportunidade fantástica para os estudantes mostrarem suas habilidades esportivas, promoverem o espírito de equipe e fortalecerem os laços de amizade entre os diferentes cursos. Que venham mais competições como essa no futuro!",
+        autor: "Talita Pereira",
+        data: "Data de Publicação: 11 de dezembro de 2020"
+    },
+    {
+        id: 5,
+        imagem: "/public/img/teatropagina5.jpg",
+        segundaImagem: "/public/img/foto5.jfif",
+        titulo: "SPF",
+        conteudo: "A Semana Paulo Freire (SPF) é sempre aguardada com grande expectativa pelos alunos da nossa escola técnica. É um momento de celebração do conhecimento, da criatividade e da colaboração entre as diferentes salas. No entanto, este ano, uma competição entre as salas trouxe à tona alguns desafios inesperados. A competição foi idealizada como uma oportunidade para os alunos demonstrarem suas habilidades em diversas áreas, desde o conhecimento técnico até a expressão artística. Cada sala estava ansiosa para mostrar seu potencial e conquistar o reconhecimento da comunidade escolar. No entanto, logo no início, surgiram problemas de parcialidade por parte de alguns professores, que favoreciam suas salas preferidas. Isso gerou um clima de descontentamento entre os alunos, que se sentiram injustiçados e desmotivados.",
+        segundoParagrafo: " Além disso, a coordenação da competição deixou a desejar, com uma administração mal organizada que priorizava os alunos favoritos para treinarem e terem mais tempo para se apresentarem. Isso gerou uma sensação de desigualdade entre as salas e minou o espírito de fair play que deveria permear o evento. Apesar dos obstáculos, muitas salas persistiram e se esforçaram para dar o seu melhor. Houve momentos de brilho e criatividade durante as apresentações, com destaque para o talento e dedicação de diversos alunos. No final, apesar dos contratempos, a competição serviu como uma lição sobre a importância da imparcialidade, da transparência e da equidade em qualquer evento escolar. Espera-se que no próximo ano, a Semana Paulo Freire seja conduzida de forma mais justa e inclusiva, para que todos os alunos tenham a oportunidade de brilhar e se sentir valorizados.",
+        autor: "Geovana Costa",
+        data: "Data de Publicação: 1 de junho de 2024"
+    }
+]
+
+
