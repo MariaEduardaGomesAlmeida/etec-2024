c16e699b4b603bf48b42d99a5c4f4c34d5770702
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2024-09-25 07:48:59 -0300

    Add files via upload

---
+++ PAM/Javascript/Aula1/Exercicio/objetos.js
@@ -0,0 +1,41 @@
+//Objetos
+
+console.log("-- Aluno 1 --");
+
+//Criando objeto Aluno 1
+var aluno1 = {
+    matricula: 534473,
+    nome: "Maria Almeida",
+    curso: "Desenvolvimento de Sistemas",
+    ativo: true
+}
+
+//Imprime valores Aluno 1
+console.log(aluno1.nome);
+console.log(aluno1.matricula);
+console.log(aluno1.curso);
+console.log(aluno1.ativo);
+
+aluno1.dataNascimento = '07/01/2008';
+console.log(aluno1.dataNascimento);
+
+
+
+console.log("-- Aluno 2 --");
+
+//Criando objeto Aluno 2
+var aluno2 = {
+    matricula: 534102,
+    nome: "Lucas Santana",
+    curso: "Desenvolvimento de Sistemas",
+    ativo: true
+}
+
+//Imprime valores Aluno 2
+console.log(aluno2.nome);
+console.log(aluno2.matricula);
+console.log(aluno2.curso);
+console.log(aluno2.ativo);
+
+aluno2.dataNascimento = '06/05/1507';
+console.log(aluno2.dataNascimento);
