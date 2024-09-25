666d696a0fc0fa72ff3fd4b467e81253783fd416
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2024-09-25 07:46:30 -0300

    Add files via upload

---
+++ PAM/Javascript/Aula1/Exemplo/objetos.js
@@ -0,0 +1,28 @@
+//Objetos
+
+var aluno1 = {
+    matricula: 75638663,
+    nome: "Alejandro Oliveira",
+    curso: "Bootcamp Front-end",
+    ativo: true
+}
+
+console.log(aluno1.nome);
+
+aluno1.dataNascimento = '07/06/1948';
+console.log(aluno1.dataNascimento);
+
+delete aluno1.dataNascimento;
+
+
+
+//Underfined e null
+
+var x;
+var y = null;
+
+
+
+//Arrays
+
+var frutas = ['Laranja', 'Limão', 'Amora'];
---
+++ PAM/Javascript/Aula1/Exemplo/primitivos.js
@@ -0,0 +1,4 @@
+ //Tipos Valores
+var vNumber = 5.78;
+var vString = 'abacaxi';
+var vBoolean = true;
