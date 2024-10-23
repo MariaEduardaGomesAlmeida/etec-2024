6bf213c54b5c0a715dcb0c31d1661231adb8860f
Maria Eduarda <143905191+MariaEduardaGomesAlmeida@users.noreply.github.com>
2024-10-23 07:49:26 -0300

    Add files via upload

---
+++ PAM/Javascript/Aula5/Exemplo/script.js
@@ -0,0 +1,24 @@
+console.log('Imprimindo de 1 a 5 com While');
+var contador1 = 1;
+while(contador1 <= 5){
+    console.log(contador1);
+    contador1++;
+}
+
+
+
+console.log('Imprimindo de 1 a 5 com DoWhile');
+var contador2 = 1;
+do{
+    console.log(contador2);
+    contador2++;
+}while(contador2 <= 5)
+
+
+
+console.log('Imprimindo de 1 a 5 com For');
+contador3 = 1;
+for(; contador3 <= 5; contador3++){
+    console.log(contador3);
+}
+
